d9e11f3c9f3a00de4bc1bf1def074672b123730f
Han JU @ KXEN <ju.han.felix@gmail.com>
2013-06-27 17:45:46 +0200

    completed a echo server between c client and scala server

--- src/main/native/powerline-client.c
+++ src/main/native/powerline-client.c
@@ -1,5 +1,6 @@
 /*
- * Native client for powerline-shell server
+ * Client for powerline-shell server
+ * Compiled native client run much faster then interpreted or vm code
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,11 +28,7 @@ int main(int argc, char **argv) {
   int sockfd, portno, n;
   struct sockaddr_in serveraddr;
   struct hostent *server;
-  char *hostname;
   char buf[BUFSIZE];
-
-  hostname = HOST;
-  portno = PORT;
 
   /* socket: create the socket */
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -39,9 +36,9 @@ int main(int argc, char **argv) {
     error("ERROR opening socket");
 
   /* gethostbyname: get the server's DNS entry */
-  server = gethostbyname(hostname);
+  server = gethostbyname(HOST);
   if (server == NULL) {
-    fprintf(stderr,"ERROR, no such host as %s\n", hostname);
+    fprintf(stderr,"ERROR, no such host as %s\n", HOST);
     exit(0);
   }
 
@@ -50,7 +47,7 @@ int main(int argc, char **argv) {
   serveraddr.sin_family = AF_INET;
   bcopy((char *)server->h_addr,
 	(char *)&serveraddr.sin_addr.s_addr, server->h_length);
-  serveraddr.sin_port = htons(portno);
+  serveraddr.sin_port = htons(PORT);
 
   /* connect: create a connection with the server */
   if (connect(sockfd, &serveraddr, sizeof(serveraddr)) < 0)
@@ -58,10 +55,14 @@ int main(int argc, char **argv) {
 
   /* get current pwd */
   char* pwd = getenv(PWD);
-  pwd[strlen(pwd)] = '\n';
+  int len = strlen(pwd);
+  char* msg = (char*)malloc((len+1)*sizeof(char));
+  strcpy(msg, pwd);
+  msg[len] = '\n';
 
   /* write: send the message line to the server */
-  n = write(sockfd, pwd, strlen(pwd));
+  n = write(sockfd, msg, strlen(msg));
+  free(msg);
   if (n < 0)
     error("ERROR writing to socket");
 
--- src/main/scala/PowerlineServer.scala
+++ src/main/scala/PowerlineServer.scala
@@ -1,24 +1,26 @@
-import java.io.{PrintStream, IOException}
+import java.io.{InputStreamReader, BufferedReader, PrintStream, IOException}
 import java.net.{SocketException, ServerSocket}
 import scala.io.BufferedSource
 
 /**
- *
+ * Powerline server that do all the job as a daemon
  */
 object PowerlineServer extends App {
+
+  val segments: List[String] = List()
+
+  // Main
   try {
     val server = new ServerSocket(18888)
     while (true) {
       val socket = server.accept()
-      val in = new BufferedSource(socket.getInputStream).getLines()
+      val in = new BufferedReader(new InputStreamReader(socket.getInputStream))
       val out = new PrintStream(socket.getOutputStream)
 
-      while (in.hasNext) {
-        val msg = in.next()
-        out.print(msg)
-        // out.print('\0')
-        println("Received: "+msg)
-      }
+      val msg = in.readLine()
+      out.print(msg)
+      println("Received: "+msg)
+
       out.close()
       socket.close()
     }
